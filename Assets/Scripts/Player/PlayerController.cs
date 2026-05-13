ea643c89e7b89799fcc39f978aafb0787f1dec3a
Xander500 <jalamoy50@gmail.com>
2026-05-13 19:40:06 -0400

    added dash, added file structure to scripts for player

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 moveInput;
+    [SerializeField] float moveSpeed;
+    Rigidbody2D rb;
+
+
+    //DASHING LOGIC
+    [SerializeField] float dashCoolDown;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashDuration;
+    Boolean canDash = true;
+    Boolean dashing = false;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        //DASHING LOGIC
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) {
+            StartCoroutine(Dash());
+        }
+
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        //DASHING LOGIC
+        if (dashing)
+            return;
+
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    //DASHING LOGIC
+    IEnumerator Dash() {
+        dashing = true;
+        canDash = false;
+        rb.linearVelocity = moveInput * dashSpeed;
+        yield return new WaitForSeconds(dashDuration);
+        dashing = false;
+        yield return new WaitForSeconds(dashCoolDown);
+        canDash = true;
+    }
+
+}
